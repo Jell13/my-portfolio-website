2dc74697647c0114cf8076bf40856ae9c62f0256
Jason Sugiharto <jasonliesugiharto@gmail.com>
2023-12-23 12:09:21 -0800

    added reveal

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-
+import Reveal from 'react-reveal'
 const About = () => {
   return (
     <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About me</p>
-        </div>
-        <p className='text-xl mt-20 '>
-          As a junior Front-End Developer, I possess the arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, Python.
-          I excel in maintaining and designing responsive websites that offers a smooth user experience in different types of medium.
-        </p>
-        <br />
-        <p className='text-xl'>
-          I am a team player who thrives in collaborating with cross functional-teams to produce an outstanding web application.
-          My free time consist of doing problem solving activities and be an active learner in the world of coding.      
-        </p>
+        <Reveal>
+          <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About me</p>
+          </div>
+          <p className='text-xl mt-20 '>
+            As a junior Front-End Developer, I possess the arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, Python.
+            I excel in maintaining and designing responsive websites that offers a smooth user experience in different types of medium.
+          </p>
+          <br />
+          <p className='text-xl'>
+            I am a team player who thrives in collaborating with cross functional-teams to produce an outstanding web application.
+            My free time consist of doing problem solving activities and be an active learner in the world of coding.      
+          </p>
+        </Reveal>
       </div>
     </div>
   )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import empty_repository from '../assets/empty_repository.png'
+import { Reveal } from 'react-reveal'
 
 
 const Project = () => {
@@ -7,56 +8,56 @@ const Project = () => {
   return (
     <div name='project' className='bg-gradient-to-b from-black to-gray-800 w-full md:h-full text-white'>
         <div className=' max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className=' text-4xl font-bold border-b-4 inline border-gray-500'>Projects</p>
-                <p className='py-6 text-xl'>Each project is a unique piece of development</p>
-            </div>
-            <div className='flex flex-col gap-12'>
-    
-                <div className='flex flex-col sm:flex-row rounded-lg'>
-                    <img className=' h-auto w-full sm:w-1/2 rounded-md duration-200 hover:scale-105 inline' src={empty_repository} alt="" />
-                    <div className=' p-8 text-center w-full sm:w-1/2 flex flex-col justify-center items-center gap-12'>
-                        <h2 className='font-bold text-lg pb-8'>First Project Title</h2>
-                        <p>Something something something
-                            something something something
-                        </p>
-                        <div className='flex justify-around w-full'>
-                            <button>Code</button>
-                            <button>Live Demo</button>
+            <Reveal>
+                <div className='pb-8'>
+                    <p className=' text-4xl font-bold border-b-4 inline border-gray-500'>Projects</p>
+                    <p className='py-6 text-xl'>Each project is a unique piece of development</p>
+                </div>
+                <div className='flex flex-col gap-12'>
+        
+                    <div className='flex flex-col sm:flex-row rounded-lg'>
+                        <img className=' h-auto w-full sm:w-1/2 rounded-md duration-200 hover:scale-105 inline' src={empty_repository} alt="" />
+                        <div className=' p-8 text-center w-full sm:w-1/2 flex flex-col justify-center items-center gap-12'>
+                            <h2 className='font-bold text-lg pb-8'>First Project Title</h2>
+                            <p>Something something something
+                                something something something
+                            </p>
+                            <div className='flex justify-around w-full'>
+                                <button>Code</button>
+                                <button>Live Demo</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col sm:flex-row-reverse rounded-lg'>
+                        <img className=' h-auto w-full sm:w-1/2 rounded-md duration-200 hover:scale-105 inline' src={empty_repository} alt="" />
+                        <div className=' p-8 text-center w-full sm:w-1/2 flex flex-col justify-center items-center gap-12'>
+                            <h2 className='font-bold text-lg pb-8'>Second Project Title</h2>
+                            <p>Something something something
+                                something something something
+                            </p>
+                            <div className='flex justify-around w-full'>
+                                <button>Code</button>
+                                <button>Live Demo</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col sm:flex-row rounded-lg'>
+                        <img className=' h-auto w-full sm:w-1/2 rounded-md duration-200 hover:scale-105 inline' src={empty_repository} alt="" />
+                        <div className=' p-8 text-center w-full sm:w-1/2 flex flex-col justify-center items-center gap-12'>
+                            <h2 className='font-bold text-lg pb-8'>Third Project Title</h2>
+                            <p>Something something something
+                                something something something
+                            </p>
+                            <div className='flex justify-around w-full'>
+                                <button>Code</button>
+                                <button>Live Demo</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-                <div className='flex flex-col sm:flex-row-reverse rounded-lg'>
-                    <img className=' h-auto w-full sm:w-1/2 rounded-md duration-200 hover:scale-105 inline' src={empty_repository} alt="" />
-                    <div className=' p-8 text-center w-full sm:w-1/2 flex flex-col justify-center items-center gap-12'>
-                        <h2 className='font-bold text-lg pb-8'>Second Project Title</h2>
-                        <p>Something something something
-                            something something something
-                        </p>
-                        <div className='flex justify-around w-full'>
-                            <button>Code</button>
-                            <button>Live Demo</button>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='flex flex-col sm:flex-row rounded-lg'>
-                    <img className=' h-auto w-full sm:w-1/2 rounded-md duration-200 hover:scale-105 inline' src={empty_repository} alt="" />
-                    <div className=' p-8 text-center w-full sm:w-1/2 flex flex-col justify-center items-center gap-12'>
-                        <h2 className='font-bold text-lg pb-8'>Third Project Title</h2>
-                        <p>Something something something
-                            something something something
-                        </p>
-                        <div className='flex justify-around w-full'>
-                            <button>Code</button>
-                            <button>Live Demo</button>
-                        </div>
-                    </div>
-                </div>
-                
-
-            </div>
+            </Reveal>
         </div>
     </div>
   )
