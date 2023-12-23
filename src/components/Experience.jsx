1895e0f4a3fa0f277824a37e86e0df330846cdf4
Jason Sugiharto <jasonliesugiharto@gmail.com>
2023-12-23 12:11:22 -0800

    remove reveal

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,7 +6,6 @@ import reactImg from '../assets/react.png'
 import tailwind from '../assets/tailwind.png'
 import github from '../assets/github.png'
 import python from '../assets/python-logo.png'
-import { Reveal } from 'react-reveal'
 
 
 const Experience = () => {
@@ -53,20 +52,18 @@ const Experience = () => {
   return (
     <div name='experience' className=' w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white '>
        <div className=' max-w-screen-lg flex flex-col justify-center mx-auto p-4 w-full h-full'>
-        <Reveal>
-                <div className='pb-8'>
-                    <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>Experience</h2>
-                    <p className='text-xl py-6'>These are the things I am experienced with</p>
-                </div>
-                <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-                    {experience.map(({id,src,text,style}) => (
-                            <div key={id} className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg' + " " + style}>
-                                <img src={src} alt="" className=' w-20 mx-auto' />
-                                <p className='mt-4'>{text}</p>
-                            </div>
-                    ))}
-                </div>
-        </Reveal>
+            <div className='pb-8'>
+                <h2 className='text-4xl font-bold inline border-b-4 border-gray-500'>Experience</h2>
+                <p className='text-xl py-6'>These are the things I am experienced with</p>
+            </div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                {experience.map(({id,src,text,style}) => (
+                        <div key={id} className={'shadow-md hover:scale-105 duration-500 py-2 rounded-lg' + " " + style}>
+                            <img src={src} alt="" className=' w-20 mx-auto' />
+                            <p className='mt-4'>{text}</p>
+                        </div>
+                ))}
+            </div>
         </div> 
     </div>
   )
